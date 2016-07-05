3e3cf28bda0fbbcc2a0f4e66add741c413bbd2c9
LordMike <lordmike@iki.fi>
2016-07-05 05:59:29 +0300

    First cuda test. Works only with Debug/Win32

--- src/console/libhologram_console.cpp
+++ src/console/libhologram_console.cpp
@@ -3,9 +3,19 @@
 
 #include "stdafx.h"
 
+#include <cuda.h>
+#include "cudaAdd.cuh"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cuInit(0);
+
+	float a = 1, b = 2, c = 0;
+	int n = 1;
+
+	vectorAddition(&a, &b, &c, n);
+	std::cout << "Result: " << c << std::endl;
+
 	system("Pause");
 	return 0;
 }
